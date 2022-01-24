2cd1b10877365d1633f29172cccedb7b36e87ceb
homewinner <77879165+homewinner@users.noreply.github.com>
2022-01-24 17:07:14 +0700

    A

---
+++ app/src/main/java/com/example/ikoniccoffee/Emp_Edit.kt
@@ -0,0 +1,11 @@
+package com.example.ikoniccoffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Emp_Edit : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_emp_edit)
+    }
+}
---
+++ app/src/main/java/com/example/ikoniccoffee/Reset.kt
@@ -0,0 +1,11 @@
+package com.example.ikoniccoffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Reset : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset)
+    }
+}
